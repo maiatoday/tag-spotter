6185434946fc89e9883319eb7a59f845b1f76109
Maia Grotepass <maiatoday@gmail.com>
2026-05-31 21:32:04 +0200

    [Chore] Add kotlinx-serialization-json dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
 
   // Core Android dependencies
   implementation(libs.androidx.core.ktx)
+  implementation(libs.kotlinx.serialization.json)
   implementation(libs.androidx.lifecycle.runtime.ktx)
   implementation(libs.androidx.activity.compose)
 
